8240ab60ce6ad066b44b9482188f602943074e45
AnderMichael <andercaymi@gmail.com>
2023-10-10 11:39:34 -0400

    First Commit

---
+++ src/test/java/task1/ItemsAdministrator.java
@@ -0,0 +1,13 @@
+package task1;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+public class ItemsAdministrator {
+    JSONObject jsonObject;
+
+    @Test
+    public void createItems(){
+
+    }
+}
